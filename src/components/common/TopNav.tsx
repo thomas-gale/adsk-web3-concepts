ddcb65e958b0619ef7281e653671c4dc18c22f1c
Thomas Gale <gale.thomasjames@gmail.com>
2022-01-13 10:03:12 +0000

    Adding button to link to github.

--- src/components/common/TopNav.tsx
+++ src/components/common/TopNav.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { GitHub } from "@mui/icons-material";
 import { config } from "../../env/config";
 import { Identity } from "./Identity";
 import { Session } from "../../types/web3/Session";
@@ -17,6 +18,9 @@ export const TopNav = (props: TopNavProps): JSX.Element => {
         <Toolbar>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img alt="Autodesk Icon" src={config.icon} height={16} width={180} />
+          <IconButton color="inherit" aria-label="github" href={config.github}>
+            <GitHub />
+          </IconButton>
           <Typography variant="h6" style={{ flexGrow: 1 }}>
             {config.appName}
           </Typography>
--- src/env/config.ts
+++ src/env/config.ts
@@ -1,6 +1,7 @@
 export const config = {
   appName: "web3 concepts",
   icon: "/autodesk-logo-primary-rgb-white.svg",
+  github: "https://github.com/thomas-gale/adsk-web3-concepts",
   api: {
     forge: {
       auth: {
@@ -18,7 +19,8 @@ export const config = {
         script:
           "https://developer.api.autodesk.com/modelderivative/v2/viewers/7.55.1/viewer3D.min.js",
         examples: {
-          gearboxGltf: "https://gateway.pinata.cloud/ipfs/Qmaz3RP6BVNNJVPvEFSRjpXyGx6cxwFHHyJUKtX8Ue4r2b/GearboxAssy.gltf",
+          gearboxGltf:
+            "https://gateway.pinata.cloud/ipfs/Qmaz3RP6BVNNJVPvEFSRjpXyGx6cxwFHHyJUKtX8Ue4r2b/GearboxAssy.gltf",
         },
       },
       translation: {

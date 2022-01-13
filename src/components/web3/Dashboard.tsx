e118124d93f79c592af82f566a2402cdf1b428ae
Thomas Gale <gale.thomasjames@gmail.com>
2022-01-13 11:26:53 +0000

    Adding manual load and unload buttons.

--- src/components/web3/Dashboard.tsx
+++ src/components/web3/Dashboard.tsx
@@ -1,42 +1,57 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef } from "react";
 import { config } from "../../env/config";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { Viewer } from "../forge/Viewer";
 import { ViewingContextProvider } from "../forge/ViewingContext";
 
 export const Dashboard = (): JSX.Element => {
-  const experimentWithGearboxModel = useCallback(
-    (viewer: Autodesk.Viewing.GuiViewer3D) => {
-      viewer.loadModel(config.api.forge.viewer.examples.gearboxGltf, {});
-    },
-    []
-  );
+  const viewer = useRef<Autodesk.Viewing.GuiViewer3D>();
+
+  const onViewerLoaded = useCallback((v: Autodesk.Viewing.GuiViewer3D) => {
+    viewer.current = v;
+  }, []);
+
+  const clearModels = useCallback(() => {
+    if (viewer.current) {
+      viewer.current.getAllModels().forEach((model) => {
+        viewer.current?.unloadModel(model);
+      });
+    }
+  }, []);
+
+  const loadModel = useCallback(() => {
+    if (viewer.current) {
+      viewer.current.loadModel(
+        config.api.forge.viewer.examples.gearboxGltf,
+        {}
+      );
+    }
+  }, []);
 
   return (
     <ViewingContextProvider options={{ env: "Local" }}>
       <Box sx={{ display: "flex", flexDirection: "column" }}>
         <Box sx={{ p: 1, m: 1 }}>
-          <p>
-            <ul>
-              <li>
-                Gearbox data (currently un-encrypted) stored on IPFS through{" "}
-                <a href={config.api.forge.viewer.examples.gearboxGltf}>
-                  gateway
-                </a>
-              </li>
-              <li>
-                Page deployed on IPFS using{" "}
-                <a href="https://fleek.co/">fleek</a>
-              </li>
-            </ul>
-          </p>
+          <Box>
+            Page deployed on IPFS using <a href="https://fleek.co/">fleek</a>
+            <Button sx={{ m: 1 }} variant="outlined" onClick={clearModels}>
+              Clear Models
+            </Button>
+          </Box>
+          <Box>
+            Gearbox data (currently un-encrypted) stored on IPFS through{" "}
+            <a href={config.api.forge.viewer.examples.gearboxGltf}>gateway</a>
+            <Button sx={{ m: 1 }} variant="outlined" onClick={loadModel}>
+              Load Model
+            </Button>
+          </Box>
         </Box>
         <Box sx={{ flexGrow: 1 }}>
           <Viewer
             config={{
               extensions: ["Autodesk.Viewing.SceneBuilder", "Autodesk.glTF"],
             }}
-            onLoaded={experimentWithGearboxModel}
+            onLoaded={onViewerLoaded}
           />
         </Box>
       </Box>

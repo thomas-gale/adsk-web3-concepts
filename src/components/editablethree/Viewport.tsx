e2d59e68ae677b4022664e5212bb47988222c846
Thomas Gale <gale.thomasjames@gmail.com>
2022-01-14 13:54:11 +0000

    Save and load working.

--- src/components/editablethree/Viewport.tsx
+++ src/components/editablethree/Viewport.tsx
@@ -1,12 +1,22 @@
-import React, { Suspense, useCallback, useState } from "react";
+import React, {
+  Suspense,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { Canvas } from "@react-three/fiber";
 import { Loader } from "@react-three/drei";
 import { Editor } from "./Editor";
 import { SceneState } from "../../types/editablethree/SceneState";
 import { Button } from "../elements/Button";
-import { Ipfs } from "../web3/Ipfs";
+import * as IPFS from "ipfs-core";
 
 export const Viewport = (): JSX.Element => {
+  const ipfsRef = useRef<IPFS.IPFS>();
+  const [nodeActive, setNodeActive] = useState(false);
+  const [loadDataCid, setLoadDataCid] = useState("");
+  const [saveDataCid, setSaveDataCid] = useState("");
   const [sceneState, setSceneState] = useState({
     nodes: [
       {
@@ -30,26 +40,69 @@ export const Viewport = (): JSX.Element => {
     ],
   } as SceneState);
 
-  const onLoad = useCallback(() => {
-    console.log("onLoad");
+  useEffect(() => {
+    (async () => {
+      const ipfs = await IPFS.create();
+      ipfsRef.current = ipfs;
+      setNodeActive(true);
+      const { cid } = await ipfs.add("Some Magic Data");
+      console.info(cid.toString());
+      // QmXXY5ZxbtuYj6DnfApLiGstzPN7fvSyigrRee3hDWPCaf
+    })();
+    return () => {
+      if (ipfsRef.current) {
+        ipfsRef.current
+          .stop()
+          .catch((err) => console.error(err))
+          .finally(() => setNodeActive(false));
+      }
+    };
   }, []);
 
-  const onSave = useCallback(() => {
+  const onLoad = useCallback(async () => {
+    console.log("onLoad");
+    if (ipfsRef.current) {
+      console.log("Loading", loadDataCid);
+      const stream = ipfsRef.current.cat(loadDataCid);
+      let data = "";
+      for await (const chunk of stream) {
+        // chunks of data are returned as a Buffer, convert it back to a string
+        data += chunk.toString();
+      }
+      console.log(data);
+    }
+  }, [loadDataCid]);
+
+  const onSave = useCallback(async () => {
     console.log("onSave");
+    if (ipfsRef.current) {
+      const { cid } = await ipfsRef.current.add("Some Magic Data");
+      setSaveDataCid(cid.toString());
+      console.info(cid.toString());
+    }
   }, []);
 
   return (
     <div className="flex flex-col w-full h-full overflow-hidden">
       <div className="z-10 absolute flex flex-col">
         <div className="flex flex-col m-4 p-4 min-w-max rounded-xl bg-dark text-light bg-opacity-90 shadow-lg">
-          <Ipfs />
+          <div>IPFS Node {nodeActive ? "✅" : "⚠️"}</div>
           <div className="flex flex-row mt-2">
+            <input
+              className="p-2 m-2 font-artifakt bg-light text-dark shadow-lg"
+              type="text"
+              value={loadDataCid}
+              onChange={(e) => setLoadDataCid(e.target.value)}
+            />
             <Button mode="light" className="mr-4" onClick={onLoad}>
               Load
             </Button>
+          </div>
+          <div className="flex flex-row mt-2">
             <Button mode="light" onClick={onSave}>
               Save
             </Button>
+            <div className="ml-2 max-w-xs break-words">{saveDataCid}</div>
           </div>
         </div>
         <div className="flex flex-col mx-4 p-4 min-w-max rounded-xl bg-dark text-light bg-opacity-90 shadow-lg">

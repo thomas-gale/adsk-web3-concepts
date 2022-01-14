05f10b9abc4082e0497020a7da0b215919ed06ed
Thomas Gale <gale.thomasjames@gmail.com>
2022-01-14 11:46:54 +0000

    WIP: Experimenting with web3-react (stable v6).

--- src/components/elements/Identity.tsx
+++ src/components/elements/Identity.tsx
@@ -1,38 +1,85 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import { Session } from "../../types/web3/Session";
 import { Button } from "./Button";
+
+import {
+  Web3ReactProvider,
+  useWeb3React,
+  UnsupportedChainIdError,
+} from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { Web3Provider } from "@ethersproject/providers";
+
+// Metamask
+export const injected = new InjectedConnector({ supportedChainIds: [1, 5] });
+
+// import { initializeConnector } from "@web3-react/core";
+// import type { Web3ReactHooks } from "@web3-react/core";
+// import type { Connector } from "@web3-react/types";
+// import { Network } from "@web3-react/network";
+// import { MetaMask } from "@web3-react/metamask";
+
+// // web3 metamask connector
+// export const [metaMask, mmHooks, mmStore] = initializeConnector<MetaMask>(
+//   (actions) => new MetaMask(actions)
+// );
 
 export interface IdentityProps {
   session: Session | undefined;
   setSession: (session: Session | undefined) => void;
 }
 
-export const Identity = ({ session }: IdentityProps): JSX.Element => {
+export const Identity = (_: IdentityProps): JSX.Element => {
   // Hooks to talk with metamask
+
+  // Network
+  // const nChainId = nHooks.useChainId();
+  // const nAccounts = nHooks.useAccounts();
+  // const nError = nHooks.useError();
+  // const nError = false;
+  // const nConnected = false;
+  // const nConnected = Boolean(nChainId && nAccounts);
+
+  const { connector, error, activate } = useWeb3React<Web3Provider>();
+
+  // useEffect(() => {
+  //   console.log(context);
+  // });
+
+  // MetaMask
 
   return (
     <div className="m-2 flex flex-row items-center">
-      {!session && (
+      <div>
+        {error ? (
+          <>🛑 Error</>
+        ) : connector ? (
+          <>✅ Connected</>
+        ) : (
+          <>⚠️ Disconnected</>
+        )}
+      </div>
+      {!connector && (
         <Button
           mode="dark"
           className="mx-2"
-          onClick={async (): Promise<void> =>
-            console.log("Linking with Metamask wallet...")
-          }
+          onClick={async (): Promise<void> => {
+            console.log("Linking with Metamask wallet...");
+            activate(injected);
+          }}
         >
-          Connect Wallet
+          Link with Metamask
         </Button>
       )}
-      {session && (
+      {connector && (
         <Button
           mode="dark"
-          className="mx-2 "
-          onClick={async (): Promise<void> =>
-            console.log("Unlinking with Metamask wallet...")
-          }
+          className="mx-2"
+          onClick={async (): Promise<void> => {
+            console.log("Unlinking with Metamask wallet...");
+          }}
         >
-          Disconnect Wallet
+          Unlink with Metamask
         </Button>
       )}
     </div>

3fdbc105a6331527a47ab0a797fc9b6ce5aaed3a
Thomas Gale <gale.thomasjames@gmail.com>
2022-01-15 21:12:28 +0000

    WIP: Deep in IPFS browser p2p weeds.

--- src/components/editablethree/Viewport.tsx
+++ src/components/editablethree/Viewport.tsx
@@ -12,11 +12,13 @@ import { SceneState } from "../../types/editablethree/SceneState";
 import { Button } from "../elements/Button";
 import * as IPFS from "ipfs-core";
 import { config } from "../../env/config";
+import { Message } from "ipfs-core-types/src/pubsub";
 
 export const Viewport = (): JSX.Element => {
   const ipfsRef = useRef<IPFS.IPFS>();
   const [nodeActive, setNodeActive] = useState(false);
   const [loadDataCid, setLoadDataCid] = useState("");
+  const [loading, setLoading] = useState(false);
   const [saveDataCid, setSaveDataCid] = useState("");
   const [sceneState, setSceneState] = useState({
     nodes: [
@@ -44,15 +46,103 @@ export const Viewport = (): JSX.Element => {
   useEffect(() => {
     (async () => {
       const ipfs = await IPFS.create({
-        repo: "adsk-web3-concepts", // should this be somehow related to the user metamask public key?
+        repo: "ok" + Math.random(), // random so we get a new peerid every time, useful for testing
+        // repo: "adsk-web3-concepts", // should this be somehow related to the user metamask public key?
         config: {
           Addresses: {
             Swarm: [config.ipfs.webRtcStarServer],
           },
+          Bootstrap: [],
+          Routing: {
+            Type: "none",
+          },
         },
       });
       ipfsRef.current = ipfs;
-      setNodeActive(true);
+
+      // console.log("stats bitswap", await ipfsRef.current.stats.bitswap());
+      // console.log("stats repo", await ipfsRef.current.stats.repo());
+      const id = await ipfsRef.current.id();
+      console.log("ipfs id", id.id);
+      // console.log("peers", await ipfsRef.current.swarm.peers());
+      // console.log("peer addrs", await ipfsRef.current.swarm.addrs());
+
+      // processes a circuit-relay announce over pubsub
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      // const processAnnounce = async (addr: any) => {
+      //   console.log("announce", addr);
+      //   // get our peerid
+      //   const me = await ipfs.id();
+      //   // me = me.id;
+
+      //   // not really an announcement if it's from us
+      //   if (addr.from == me.id) {
+      //     return;
+      //   }
+
+      //   // if we got a keep-alive, nothing to do
+      //   if (addr == "keep-alive") {
+      //     console.log(addr);
+      //     return;
+      //   }
+
+      //   const peer = addr.split("/")[9];
+      //   console.log("Peer: " + peer);
+      //   console.log("Me: " + me);
+      //   if (peer == me) {
+      //     // return if the peer being announced is us
+      //     return;
+      //   }
+
+      //   // get a list of peers
+      //   const peers = await ipfs.swarm.peers();
+      //   for (const i in peers) {
+      //     // if we're already connected to the peer, don't bother doing a
+      //     // circuit connection
+      //     if (peers[i].peer == peer) {
+      //       return;
+      //     }
+      //   }
+      //   // log the address to console as we're about to attempt a connection
+      //   console.log(addr);
+
+      //   // connection almost always fails the first time, but almost always
+      //   // succeeds the second time, so we do this:
+      //   try {
+      //     await ipfs.swarm.connect(addr);
+      //   } catch (err) {
+      //     console.log(err);
+      //     await ipfs.swarm.connect(addr);
+      //   }
+      // };
+
+      // // process announcements over the relay network, and publish our own
+      // // keep-alives to keep the channel alive
+      // await ipfs.pubsub.subscribe("announce-circuit", processAnnounce);
+      // setInterval(function () {
+      //   ipfs.pubsub.publish("announce-circuit", "peer-alive");
+      // }, 15000);
+
+      // Handshaking
+      ipfsRef.current.pubsub.subscribe("handshake", (message: Message) => {
+        // Ignore from me
+        if (message.from == id.id) return;
+
+        console.log("handshake from ", message.from);
+        if (ipfsRef.current) {
+          ipfsRef.current.swarm.peers().then((peers) => console.log(peers));
+          ipfsRef.current.swarm.addrs().then((addrs) => console.log(addrs));
+          // ipfsRef.current.swarm.connect(message.1);
+          // ipfsRef.current.pin.remote.service.add()
+          setNodeActive(true);
+        }
+      });
+      setInterval(function () {
+        ipfs.pubsub.publish(
+          "handshake",
+          new TextEncoder().encode(`hello from ${id.id}`)
+        );
+      }, 2000);
     })();
     return () => {
       if (ipfsRef.current) {
@@ -66,7 +156,7 @@ export const Viewport = (): JSX.Element => {
 
   const onLoad = useCallback(async () => {
     if (ipfsRef.current) {
-      console.log("Loading", loadDataCid);
+      setLoading(true);
       const stream = ipfsRef.current.cat(loadDataCid);
       let data = "";
       for await (const chunk of stream) {
@@ -74,13 +164,15 @@ export const Viewport = (): JSX.Element => {
         data += chunk.toString();
       }
       setSceneState(JSON.parse(data));
+      setLoading(false);
     }
   }, [loadDataCid]);
 
   const onSave = useCallback(async () => {
     if (ipfsRef.current) {
       const { cid } = await ipfsRef.current.add(JSON.stringify(sceneState));
-      setSaveDataCid(cid.toString());
+      const pinnedCid = await ipfsRef.current.pin.add(cid);
+      setSaveDataCid(pinnedCid.toString());
     }
   }, [sceneState]);
 
@@ -99,6 +191,7 @@ export const Viewport = (): JSX.Element => {
             <Button mode="light" className="mr-4" onClick={onLoad}>
               Load
             </Button>
+            <div>{loading ? "loading" : ""}</div>
           </div>
           <div className="flex flex-row mt-2">
             <Button mode="light" onClick={onSave}>

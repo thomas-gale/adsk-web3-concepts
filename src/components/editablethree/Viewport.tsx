6cd9cc9c75dccb17b2beca9fc9accef527647a58
Thomas Gale <gale.thomasjames@gmail.com>
2022-01-17 09:32:36 +0000

    WIP: Open test

--- src/components/editablethree/Viewport.tsx
+++ src/components/editablethree/Viewport.tsx
@@ -25,11 +25,12 @@ export const Viewport = (): JSX.Element => {
   const ipfsRef = useRef<IPFS.IPFS>();
   const orbitDbRef = useRef<OrbitDB>();
   const kvDbRef = useRef<KeyValueStore<string>>(); // Always string key, we specify string value
+  const [kvDbAddr, setKvDbAddr] = useState("");
   const [nodeActive, setNodeActive] = useState(false);
-  const [loadDataCid, setLoadDataCid] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [newing, setNewing] = useState(false);
+  const [kvDbAddrToOpen, setKvDbAddrToOpen] = useState("");
+  const [opening, setOpening] = useState(false);
   const [saving, setSaving] = useState(false);
-  const [saveDataCid, setSaveDataCid] = useState("");
   const [sceneState, setSceneState] = useState({
     nodes: [
       {
@@ -55,8 +56,7 @@ export const Viewport = (): JSX.Element => {
 
   useEffect(() => {
     (async () => {
-      // Orbit DB experiments
-
+      // Create IPFS node
       ipfsRef.current = await IPFS.create({
         repo: "peer" + Math.random(), // Testing, this node is new peer on each mount
         config: {
@@ -66,147 +66,11 @@ export const Viewport = (): JSX.Element => {
           Bootstrap: [],
         },
       });
-
-      // Show Peer ID
-      // if (ipfsRef.current) {
-      //   const id = await ipfsRef.current.id();
-      //   console.log("IPFS ID:", id);
-      // }
-
       // Create OrbitDB instance
       orbitDbRef.current = await OrbitDB.createInstance(ipfsRef.current);
 
-      // Create database instance
-      kvDbRef.current = await orbitDbRef.current.keyvalue<string>("scenekv", {
-        accessController: {
-          write: ["*"], // For testing any peer can write
-        },
-      });
-      console.log(kvDbRef.current.address.toString());
-
-      // Db events logged
-      kvDbRef.current.events.on("replicated", (address) => {
-        console.log("Db replicated (push)", address);
-      });
-
-      kvDbRef.current.events.on("replicate", (address) => {
-        console.log("Db replicate (pull)", address);
-      });
-
-      kvDbRef.current.events.on("ready", (dbname, heads) => {
-        console.log("Db ready", dbname, heads);
-      });
-
-      kvDbRef.current.events.on("peer", (peer) => {
-        console.log("Db peer connected", peer);
-      });
-
-      // Where to add this?
+      // TODO - move
       setNodeActive(true);
-
-      // const ipfs = await IPFS.create({
-      //   repo: "ok" + Math.random(), // random so we get a new peerid every time, useful for testing
-      //   // repo: "adsk-web3-concepts", // should this be somehow related to the user metamask public key?
-      //   config: {
-      //     // Identity: {
-      //     // PeerID: "peer" + Math.random(),
-      //     // PrivKey: "priv" + Math.random(),
-      //     // },
-      //     Addresses: {
-      //       Swarm: [config.ipfs.webRtcStarServer],
-      //     },
-      //     Bootstrap: [],
-      //     // Bootstrap: [
-      //     //   "/ip4/127.0.0.1/tcp/44005/p2p/12D3KooWKaiCtkjaHEi747QpcxCp3co3Xqq34J1hH1vnW9vuQP2a",
-      //     // ],
-      //     // Routing: {
-      //     // Type: "none",
-      //     // },
-      //   },
-      // });
-      // ipfsRef.current = ipfs;
-      // console.log("stats bitswap", await ipfsRef.current.stats.bitswap());
-      // console.log("stats repo", await ipfsRef.current.stats.repo());
-      // const id = await ipfsRef.current.id();
-      // console.log("ipfs id", id.id);
-      // console.log("peers", await ipfsRef.current.swarm.peers());
-      // console.log("peer addrs", await ipfsRef.current.swarm.addrs());
-      // processes a circuit-relay announce over pubsub
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      // const processAnnounce = async (addr: any) => {
-      //   console.log("announce", addr);
-      //   // get our peerid
-      //   const me = await ipfs.id();
-      //   // me = me.id;
-      //   // not really an announcement if it's from us
-      //   if (addr.from == me.id) {
-      //     return;
-      //   }
-      //   // if we got a keep-alive, nothing to do
-      //   if (addr == "keep-alive") {
-      //     console.log(addr);
-      //     return;
-      //   }
-      //   const peer = addr.split("/")[9];
-      //   console.log("Peer: " + peer);
-      //   console.log("Me: " + me);
-      //   if (peer == me) {
-      //     // return if the peer being announced is us
-      //     return;
-      //   }
-      //   // get a list of peers
-      //   const peers = await ipfs.swarm.peers();
-      //   for (const i in peers) {
-      //     // if we're already connected to the peer, don't bother doing a
-      //     // circuit connection
-      //     if (peers[i].peer == peer) {
-      //       return;
-      //     }
-      //   }
-      //   // log the address to console as we're about to attempt a connection
-      //   console.log(addr);
-      //   // connection almost always fails the first time, but almost always
-      //   // succeeds the second time, so we do this:
-      //   try {
-      //     await ipfs.swarm.connect(addr);
-      //   } catch (err) {
-      //     console.log(err);
-      //     await ipfs.swarm.connect(addr);
-      //   }
-      // };
-      // // process announcements over the relay network, and publish our own
-      // // keep-alives to keep the channel alive
-      // await ipfs.pubsub.subscribe("announce-circuit", processAnnounce);
-      // setInterval(function () {
-      //   ipfs.pubsub.publish("announce-circuit", "peer-alive");
-      // }, 15000);
-
-      // Handshaking
-      // if (ipfsRef.current) {
-      // ipfsRef.current.pubsub.subscribe("handshake", (message: Message) => {
-      //   // Ignore from me
-      //   if (message.from == id.id) return;
-      //   console.log("handshake from ", message.from);
-      //   // if (ipfsRef.current) {
-      //   //   // ipfsRef.current.swarm.peers().then((peers) => console.log(peers));
-      //   //   ipfsRef.current.swarm.addrs().then((addrs) => console.log(addrs));
-      //   //   ipfsRef.current.swarm.connect(message.1);
-      //   //   ipfsRef.current.pin.remote.service.add()
-      //   //   setNodeActive(true);
-      //   // }
-      // });
-      // setInterval(function () {
-      //   ipfs.pubsub.publish(
-      //     "handshake",
-      //     new TextEncoder().encode(`hello from ${id.id}`)
-      //   );
-      // }, 2000);
-      // if (ipfsRef.current) {
-      //   while ((await ipfsRef.current.swarm.peers()).length === 0) {
-      //     await new Promise((resolve) => setTimeout(resolve, 1000));
-      //   }
-      //   setNodeActive(true);
-      // }
     })();
     return () => {
       if (kvDbRef.current) {
@@ -224,29 +88,100 @@ export const Viewport = (): JSX.Element => {
     };
   }, []);
 
-  const onLoad = useCallback(async () => {
+  const onNew = useCallback(async () => {
     if (ipfsRef.current) {
-      // const stream = ipfsRef.current.cat(loadDataCid);
-      // let data = "";
-      // for await (const chunk of stream) {
-      //   // chunks of data are returned as a Buffer, convert it back to a string
-      //   data += chunk.toString();
-      // }
+      if (orbitDbRef.current) {
+        setNewing(true);
+        console.log("Creating new database instance");
 
-      // console.log("Setting database instance");
+        // Create database instance
+        kvDbRef.current = await orbitDbRef.current.keyvalue<string>("scenekv", {
+          accessController: {
+            write: ["*"], // For testing any peer can write
+          },
+        });
 
-      if (kvDbRef.current) {
-        setLoading(true);
-        console.log("Retriving state...");
-        console.log("replication", kvDbRef.current.replicationStatus);
-        const state = await kvDbRef.current.get("state");
-        if (state) {
-          console.log("Retrieved state!", state);
-          setSceneState(JSON.parse(state));
-          setLoading(false);
-        } else {
-          console.error("No state found! Try again...?");
+        // Set the database address and log
+        setKvDbAddr(kvDbRef.current.address.toString());
+        console.log(kvDbRef.current.address.toString());
+
+        // Db events logged (DUP'd below)
+        kvDbRef.current.events.on("replicated", async (address) => {
+          console.log("Db replicated (complete)", address);
+          if (kvDbRef.current) {
+            console.log("Retriving state...");
+            const state = await kvDbRef.current.get("state");
+            if (state) {
+              console.log("Retrieved state!", state);
+              setSceneState(JSON.parse(state));
+            } else {
+              console.error("No state found!");
+            }
+          }
+        });
+
+        kvDbRef.current.events.on("replicate", (address) => {
+          console.log("Db replicate (pending)", address);
+        });
+
+        kvDbRef.current.events.on("ready", (dbname, heads) => {
+          console.log("Db ready", dbname, heads);
+        });
+
+        kvDbRef.current.events.on("peer", (peer) => {
+          console.log("Db peer connected", peer);
+        });
+        setNewing(false);
+      }
+    }
+  }, []);
+
+  const onOpen = useCallback(async () => {
+    if (ipfsRef.current) {
+      if (orbitDbRef.current) {
+        setOpening(true);
+
+        if (kvDbRef.current) {
+          console.log("Closing existing database instance...");
+          await kvDbRef.current.close();
+          console.log("Existing database instance closed!");
         }
+
+        console.log("Opening database instance...");
+
+        kvDbRef.current = (await orbitDbRef.current.open(
+          kvDbAddr,
+          {}
+        )) as KeyValueStore<string>;
+
+        console.log("Database instance opened!");
+
+        // Db events logged (DUP'd below)
+        kvDbRef.current.events.on("replicated", async (address) => {
+          console.log("Db replicated (complete)", address);
+          if (kvDbRef.current) {
+            console.log("Retriving state...");
+            const state = await kvDbRef.current.get("state");
+            if (state) {
+              console.log("Retrieved state!", state);
+              setSceneState(JSON.parse(state));
+            } else {
+              console.error("No state found!");
+            }
+          }
+        });
+
+        kvDbRef.current.events.on("replicate", (address) => {
+          console.log("Db replicate (pending)", address);
+        });
+
+        kvDbRef.current.events.on("ready", (dbname, heads) => {
+          console.log("Db ready", dbname, heads);
+        });
+
+        kvDbRef.current.events.on("peer", (peer) => {
+          console.log("Db peer connected", peer);
+        });
       }
     }
   }, []);
@@ -274,23 +209,28 @@ export const Viewport = (): JSX.Element => {
         <div className="flex flex-col m-4 p-4 min-w-max rounded-xl bg-dark text-light bg-opacity-90 shadow-lg">
           <div>IPFS Node {nodeActive ? "✅" : "⚠️"}</div>
           <div className="flex flex-row mt-2">
+            <Button mode="light" className="mr-4" onClick={onNew}>
+              New Db
+            </Button>
+            <div>{newing ? "loading..." : ""}</div>
+            <div className="ml-2 max-w-xs break-words">{kvDbAddr}</div>
+          </div>
+          <div className="flex flex-row mt-2">
             <input
               className="p-2 m-2 font-artifakt bg-light text-dark shadow-lg"
               type="text"
-              value={loadDataCid}
-              onChange={(e) => setLoadDataCid(e.target.value)}
+              value={kvDbAddr}
+              onChange={(e) => setKvDbAddr(e.target.value)}
             />
-            <Button mode="light" className="mr-4" onClick={onLoad}>
-              Load
+            <Button mode="light" className="mr-4" onClick={onOpen}>
+              Open Db
             </Button>
-            <div>{loading ? "loading..." : ""}</div>
+            <div>{opening ? "opening..." : ""}</div>
           </div>
           <div className="flex flex-row mt-2">
             <Button mode="light" onClick={onSave}>
               Save
             </Button>
-            <div>{saving ? "saving..." : ""}</div>
-            <div className="ml-2 max-w-xs break-words">{saveDataCid}</div>
           </div>
         </div>
         <div className="flex flex-col mx-4 p-4 min-w-max rounded-xl bg-dark text-light bg-opacity-90 shadow-lg">
